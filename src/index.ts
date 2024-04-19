f56b5d244c4f89712eb4d43f8e5ab064aeed27fc
Kushagra <skushagra.sharma@gmail.com>
2024-04-19 11:50:46 +0530

    made the system scalable

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { WebSocketServer } from 'ws';
 import http from 'http';
-import { createClient } from "redis";
 import { v4 as uuidv4 } from 'uuid';
-import { subscribeToChannel, unsubscribeFromChannel } from './subscriber';
+import { RedisClient } from './subscriber';
 
 const server = http.createServer(function(request: any, response: any) {
   console.log((new Date()) + ' Received request for ' + request.url);
@@ -11,26 +10,41 @@ const server = http.createServer(function(request: any, response: any) {
 
 const wss = new WebSocketServer({ server });
 
-wss.on('connection', async function connection(ws) {
-  const client = createClient();
-  await client.connect()
-  const id = uuidv4()
+const users: {
+  [key: string]: {
+    room: string;
+    ws: any;
+  }
+} = {};
 
-  subscribeToChannel(id, ws);
+wss.on('connection', async function connection(ws) {
+  const id = uuidv4()
 
   ws.on('error', console.error);
 
-  ws.on('message', function message(data, isBinary) {
-    client.publish(id, JSON.stringify({ data }));
+  ws.on('message', (message) => {
+    const data = JSON.parse(message.toString());
+    if (data.type === "join") {
+      users[id] = {
+        room: data.payload.roomId,
+        ws
+      };
+      RedisClient.getInstance().subscribe(id, data.payload.roomId, ws);
+    }
+
+    if (data.type === "message") {
+      const roomId = users[id].room;
+      const message = data.payload.message;
+      RedisClient.getInstance().publish(roomId, message);
+    }
+
   });
 
   ws.send(`Hello! Message From Server!!. id is ${id}`);
 
   ws.on('close', () => {
     console.log('Client disconnected');
-    unsubscribeFromChannel();
-    client.unsubscribe();
-    client.quit();
+    RedisClient.getInstance().unsubscribe(id, users[id].room);
   });
 
 });
@@ -39,4 +53,3 @@ server.listen(8080, function() {
 });
 
 //v1 - what happening was the response was sent to all the users connected to the ws - now the response would only be sent to the person who has entered the message
-
--- src/subscriber.ts
+++ src/subscriber.ts
@@ -1,20 +1,70 @@
 import WebSocket from "ws";
-import { createClient } from "redis";
+import { createClient, RedisClientType } from "redis";
 
-const client = createClient();
+export class RedisClient {
+  private static instance: RedisClient;
+  private subscriber: RedisClientType;
+  public publisher: RedisClientType;
+  public subscriptions: Map<string, string[]>
+  public reverseSubscriptions: Map<string, { [userId: string]: { userId: string; ws: WebSocket; } }>
 
-const subscribeToChannel = (channel: string, ws: WebSocket) => {
-  client.subscribe(channel, function(messages, channel) {
-    const messageData = JSON.parse(messages).data;
-    const message = Buffer.from(messageData.data).toString('utf-8');
-    console.log(`Received message from channel ${channel}: ${message}`);
-    ws.send(message)
-  });
-};
+  private constructor() {
+    this.subscriber = createClient();
+    this.publisher = createClient();
+    this.publisher.connect()
+    this.subscriber.connect()
+    this.subscriptions = new Map<string, string[]>
+    this.reverseSubscriptions = new Map<string, { [userId: string]: { userId: string; ws: WebSocket; } }>
+  }
 
-const unsubscribeFromChannel = () => {
-  client.unsubscribe();
-};
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new RedisClient();
+    }
+    return this.instance
+  }
 
-export { subscribeToChannel, unsubscribeFromChannel };
+  subscribe(userId: string, room: string, ws: WebSocket) {
+    this.subscriptions.set(userId, [
+      ...(this.subscriptions.get(userId) || []),
+      room
+    ])
+    this.reverseSubscriptions.set(room, {
+      ...(this.reverseSubscriptions.get(room) || {}),
+      [userId]: { userId, ws }
+    })
 
+    if (Object.keys(this.reverseSubscriptions.get(room) || {}).length === 1) {
+      console.log(`First User came, Now subscribing to the room - ${room}`)
+      this.subscriber.subscribe(room, (payload) => {
+        try {
+          Object.values(this.reverseSubscriptions.get(room) || {}).forEach(({ ws }) => {
+            ws.send(payload)
+          })
+        } catch (e) {
+          console.log('error in the payload')
+        }
+      })
+    }
+  }
+
+  unsubscribe(userId: string, roomToUnsubscribe: string) {
+    this.subscriptions.set(userId,
+      this.subscriptions.get(userId)?.filter((room) => room !== roomToUnsubscribe) || []
+    )
+    if (this.subscriptions.get(userId)?.length === 0) {
+      this.subscriptions.delete(userId)
+    }
+    delete this.reverseSubscriptions.get(roomToUnsubscribe)?.[userId];
+    if (Object.keys(this.reverseSubscriptions.get(roomToUnsubscribe) || []).length === 0) {
+      console.log(`Deleting this room - ${roomToUnsubscribe} as there are no users left inside it`)
+      this.subscriber.unsubscribe(roomToUnsubscribe)
+      this.reverseSubscriptions.delete(roomToUnsubscribe)
+    }
+  }
+
+  publish(room: string, message: any) {
+    console.log(`publishing message to ${room}`);
+    this.publisher.publish(room, message);
+  }
+}
